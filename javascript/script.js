25d75ea65b4e528997aac8e92db6d84b4153beaf
Sovann-Digital <142609803+Sovann-Digital@users.noreply.github.com>
2023-09-10 16:29:04 +0700

    s

--- javascript/script.js
+++ javascript/script.js
@@ -3,5 +3,29 @@ document.addEventListener("DOMContentLoaded", () => {
     if (logo) {
         logo.innerHTML = "SOVANN";
     }
+    const navLinks = document.querySelectorAll(".nav-link");
 
+    // Add click event listeners to each navigation link
+    navLinks.forEach((link) => {
+        link.addEventListener("click", (e) => {
+            e.preventDefault();
+
+            // Get the target section's ID from the link's href attribute
+            const targetId = link.getAttribute("href").substring(1);
+
+            // Find the target section by ID
+            const targetSection = document.getElementById(targetId);
+
+            if (targetSection) {
+                // Calculate the target position relative to the viewport
+                const targetPosition = targetSection.getBoundingClientRect().top + window.scrollY;
+
+                // Scroll smoothly to the target position
+                window.scrollTo({
+                    top: targetPosition,
+                    behavior: "smooth",
+                });
+            }
+        });
+    });
 });
